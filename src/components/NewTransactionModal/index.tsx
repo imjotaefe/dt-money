210a785938bcd2fb1ffc8b88f345a5e3b2b75641
João Fernando <jotaefegds@gmail.com>
2024-08-09 11:28:19 -0300

    feat: add transaction type button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+import { ArrowCircleDown, X } from 'phosphor-react';
+import { ArrowCircleUp } from 'phosphor-react';
 
  export function NewTransactionModal(){
     return (
@@ -15,6 +16,21 @@ import { X } from 'phosphor-react';
             <input type="text" placeholder='Descrição' required />
             <input type="text" placeholder='Preço' required />
             <input type="text" placeholder='Categoria' required />
+
+            <TransactionType>
+
+              <TransactionTypeButton variant="income" value="income">
+                <ArrowCircleUp size={24}/>
+                Entrada
+              </TransactionTypeButton>
+
+              <TransactionTypeButton variant="outcome" value="outcome">
+                <ArrowCircleDown size={24}/>
+                Saída
+              </TransactionTypeButton>
+
+            </TransactionType>
+
             <button type='submit'>Cadastrar</button>
           </form>
         </Content>
