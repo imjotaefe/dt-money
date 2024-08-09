6c25d783814b367c85b2c6bf5e896f2529a10a30
João Fernando <jotaefegds@gmail.com>
2024-08-09 10:49:26 -0300

    feat: add modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTranssactionButton } from "./styles";
 import logoImg from '../../assets/Logo.svg';
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} />
-        <NewTranssactionButton> Nova Transação</NewTranssactionButton>
+        
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTranssactionButton> Nova Transação</NewTranssactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
